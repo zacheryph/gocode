389b35af74600680e1f340bd1a0d4829f9ae12f7
Zachery Hostens <zacheryph@gmail.com>
2016-01-20 23:00:27 -0600

    add initial loan/mortgage calculator

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # gocode
-General repository for examples, PoC's, small pieces of code that don't deserve a repository of their own.
+
+General repository for examples, PoC's, small pieces of code that don't deserve
+a repository of their own.
+
+## fizzbuzz
+
+Not much explaination needed here.  I doubt it will be touched again.
+
+## loan
+
+calculator for loan payments.  can print amortization calendar.  It even lets you
+check what larger payments each month will cut your loan down to
+
+* http://banking.about.com/od/loans/a/calculate_loan_interest.htm
+* http://banking.about.com/od/loans/a/loan_payment_calculations.htm
---
+++ loan/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math"
+)
+
+//
+// http://banking.about.com/od/loans/a/loan_payment_calculations.htm
+// - payment = amount / discount
+// - periodic_interest = interest / 12
+// - discount = (((1 + interest) ^payments ) - 1) / (interest(1 + interest)^payments)
+//  {[ (1 + i) ^n] - 1} / [i(1 + i)^n]
+
+var (
+	amount   = flag.Float64("amount", 0.0, "loan amount")
+	interest = flag.Float64("rate", 0.0, "interest rate")
+	months   = flag.Int("months", 360, "length of the loan (360 = 30 years)")
+)
+
+func main() {
+	flag.Parse()
+	if *amount == 0 || *interest == 0 {
+		flag.Usage()
+		return
+	}
+
+	discount := calculateDiscount(*interest/100, *months)
+	payment := *amount / discount
+
+	fmt.Printf("Monthly Payment: $%.2f\n", payment)
+
+	periodic := (*interest / 100) / 12
+	interestPayment := *amount * periodic
+	fmt.Printf("First Interest Payment: $%.2f\n", interestPayment)
+	fmt.Printf("First Principal Payment: $%.2f\n", payment-interestPayment)
+
+}
+
+func calculateDiscount(interest float64, months int) float64 {
+	periodic := interest / 12
+	daily := math.Pow(periodic+1, 360)
+	return (daily - 1) / (periodic * daily)
+}
