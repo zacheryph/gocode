581b16a407f5590d046dccffd204fd14000840e4
Zachery Hostens <zacheryph@gmail.com>
2016-02-01 21:44:32 -0600

    small package for keeping rsvp lists

---
+++ rsvp/data.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/boltdb/bolt"
+)
+
+// Rsvp defines the reservation to encode for the database
+type Rsvp struct {
+	ID    uint64
+	Name  string
+	Email string
+}
+
+func itob(v uint64) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
+}
+
+func addRsvp(db *bolt.DB, name, email string) {
+	err := db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(bucketName))
+		if bucket == nil {
+			fmt.Println("Failed to find bucket")
+			return errors.New("Bucket not found")
+		}
+
+		nextID, err := bucket.NextSequence()
+		if err != nil {
+			fmt.Println("Failed to retrieve next sequence", err)
+			return err
+		}
+
+		rsvp := Rsvp{nextID, name, email}
+		data, err := json.Marshal(rsvp)
+		if err != nil {
+			fmt.Println("Failed to marshal rsvp:", err)
+			return err
+		}
+
+		err = bucket.Put(itob(nextID), data)
+
+		return err
+	})
+
+	if err != nil {
+		fmt.Println("Failed to add RSVP:", err)
+		return
+	}
+
+	fmt.Println("Successfully added RSVP")
+}
+
+func listRsvp(db *bolt.DB) {
+	wr := tabwriter.NewWriter(os.Stdout, 0, 4, 4, ' ', 0)
+	wr.Write([]byte("Name\tEmail\n"))
+
+	db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(bucketName))
+		if bucket == nil {
+			fmt.Println("Failed to find bucket")
+			return errors.New("Bucket not found")
+		}
+
+		bucket.ForEach(func(k, v []byte) error {
+			var rsvp Rsvp
+			err := json.Unmarshal(v, &rsvp)
+			if err != nil {
+				fmt.Println("Failed to unmarshal RSVP", err)
+				return err
+			}
+
+			line := fmt.Sprintf("%s\t%s\n", rsvp.Name, rsvp.Email)
+			wr.Write([]byte(line))
+
+			return nil
+		})
+
+		return nil
+	})
+
+	wr.Flush()
+}
---
+++ rsvp/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/boltdb/bolt"
+)
+
+const bucketName = "rsvps"
+
+var (
+	db     *bolt.DB
+	dbFile = flag.String("db", "./rsvp.boltdb", "database file")
+	add    = flag.Bool("add", false, "add an rsvp to the database")
+	list   = flag.Bool("list", false, "list rsvp's in the database")
+	http   = flag.String("http", "", "start http server ([host]:port)")
+	name   = flag.String("name", "", "name of person to add to database")
+	email  = flag.String("email", "", "email of person to add to database")
+)
+
+func main() {
+	var err error
+
+	flag.Parse()
+	if !*add && !*list && *http == "" {
+		fmt.Println("Error: one of the following must be given: -add, -list, -http")
+		flag.Usage()
+		return
+	}
+
+	if *add && (*name == "" || *email == "") {
+		fmt.Println("Error: both name and email must be given for adding to the database")
+		flag.Usage()
+		return
+	}
+
+	db, err = bolt.Open(*dbFile, 0600, nil)
+	if err != nil {
+		fmt.Println("Failed to open Database:", err)
+		return
+	}
+	defer db.Close()
+
+	db.Update(func(tx *bolt.Tx) (err error) {
+		_, err = tx.CreateBucketIfNotExists([]byte("rsvps"))
+		if err != nil {
+			fmt.Println("Failed to create bucket:", err)
+		}
+
+		return err
+	})
+
+	if *add {
+		addRsvp(db, *name, *email)
+	} else if *list {
+		listRsvp(db)
+		// } else if *http != "" {
+		// 	httpServer()
+	}
+}
