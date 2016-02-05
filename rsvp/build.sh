db6583752ca7afe06da4dbf676095d0f8bf086ff
Zachery Hostens <zacheryph@gmail.com>
2016-02-04 21:28:02 -0600

    add build script

---
+++ rsvp/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+_build() {
+  container_path=/usr/src/rsvp
+
+  docker run --rm \
+    -v ${PWD}:$container_path \
+    -w $container_path \
+    golang:1.5.3-alpine \
+      go build -v
+}
+
+case "${1:-default}" in
+  default)
+    _build
+    ;;
+  build)
+    _build
+    ;;
+  vet)
+    go vet
+    ;;
+  *)
+    echo "Unknown command: $1"
+    echo "Available: build, vet"
+esac
