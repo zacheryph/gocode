54a90a622b1af4fdb63d62d169f2b60344d730b9
Tommi Virtanen <tv@eagain.net>
2016-04-05 16:31:59 -0700

    Clean up buffer management

--- cgotest/bufferfill/main.go
+++ cgotest/bufferfill/main.go
@@ -7,47 +7,108 @@ package main
 */
 import "C"
 import (
-	"bytes"
+	"syscall"
 	"unsafe"
 )
-
-type group C.struct_group
 
 func main() {
 	C.cEntryPoint()
 }
 
-//export passingGrpBuffer
-func passingGrpBuffer(grp *group, buf *C.char, buflen C.int) {
-	ourGid := 52
-	byteArr := C.GoBytes(unsafe.Pointer(buf), buflen)
-
-	// this parts easy
-	grp.gr_gid = C.gid_t(ourGid)
-
-	// using copy
-	// copy(byteArr[0:], []byte("name"))
-	// copy(byteArr[5:], []byte("passwd"))
-
-	byteBuf := bytes.NewBuffer(byteArr[:0])
-	for _, s := range []string{"name", "passwd", "one", "two", "three"} {
-		byteBuf.WriteString(s)
-		byteBuf.WriteByte(0)
+// align buffer to begin at an address that is modulo n. n must be a
+// power of two.
+func align(b []byte, n uintptr) []byte {
+	off := uintptr(unsafe.Pointer(&b[0])) % n
+	if off == 0 {
+		return b
 	}
+	if n-off > uintptr(len(b)) {
+		return nil
+	}
+	b = b[n-off:]
+	return b
+}
 
-	// memPtr := []*C.char{
-	// 	(*C.char)(unsafe.Pointer(&byteArr[12])),
-	// 	(*C.char)(unsafe.Pointer(&byteArr[16])),
-	// 	(*C.char)(unsafe.Pointer(&byteArr[20])),
-	// }
-	//
-	// TODO: the troubling part. writing back to the buffer
-	// This dont work obviously.
-	// byteBuf.Write([]byte(memPtr))
-	// byteBuf.WriteByte(0)
+// buffer manages filling a buffer from both ends: strings go in the
+// back, pointers go in the front. If the buffer runs out of space,
+// err is set, and further operations are ignored.
+type buffer struct {
+	err syscall.Errno
+	b   []byte
+}
 
-	grp.gr_name = (*C.char)(unsafe.Pointer(&byteArr[0]))
-	grp.gr_passwd = (*C.char)(unsafe.Pointer(&byteArr[5]))
-	grp.gr_gid = C.gid_t(ourGid)
-	grp.gr_mem = (**C.char)(unsafe.Pointer(&byteArr[26]))
+func newBuffer(b []byte) *buffer {
+	buf := &buffer{
+		b: align(b, unsafe.Alignof((*C.char)(nil))),
+	}
+	if buf.b == nil {
+		buf.err = syscall.ERANGE
+	}
+	return buf
+}
+
+func (b *buffer) putString(s string) *C.char {
+	if b.err != 0 {
+		return nil
+	}
+	need := len(s) + 1
+	if len(b.b) < need {
+		b.err = syscall.ERANGE
+		return nil
+	}
+	p := (*C.char)(unsafe.Pointer(&b.b[len(b.b)-need]))
+	copy(b.b[len(b.b)-need:], s)
+	b.b[len(b.b)-1] = 0x00
+	b.b = b.b[:len(b.b)-need]
+	return p
+}
+
+func (b *buffer) putPointer(p *C.char) {
+	if b.err != 0 {
+		return
+	}
+	const need = unsafe.Sizeof(p)
+	if uintptr(len(b.b)) < need {
+		b.err = syscall.ERANGE
+		return
+	}
+	dst := (**C.char)(unsafe.Pointer(&b.b[0]))
+	*dst = p
+	b.b = b.b[need:]
+	return
+}
+
+func (b *buffer) ptrToHead() **C.char {
+	if b.err != 0 {
+		return nil
+	}
+	return (**C.char)(unsafe.Pointer(&b.b[0]))
+}
+
+func fillGroup(dst *C.struct_group, dstbuf []byte, name, passwd string, gid C.__gid_t, groups []string) syscall.Errno {
+	buf := newBuffer(dstbuf)
+	dst.gr_mem = buf.ptrToHead()
+	dst.gr_name = buf.putString(name)
+	dst.gr_passwd = buf.putString(passwd)
+	dst.gr_gid = gid
+
+	for _, g := range groups {
+		p := buf.putString(g)
+		buf.putPointer(p)
+	}
+	buf.putPointer(nil)
+	return buf.err
+}
+
+//export passingGrpBuffer
+func passingGrpBuffer(dst *C.struct_group, dstbuf *C.char, dstbuflen C.int, errnop *C.int) C.int {
+	b := C.GoBytes(unsafe.Pointer(dstbuf), dstbuflen)
+	err := fillGroup(dst, b, "name", "passwd", 52, []string{
+		"one", "two", "three",
+	})
+	if err != 0 {
+		*errnop = C.int(err)
+		return -1
+	}
+	return 0
 }
