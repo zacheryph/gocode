969c6013f8b61b09a63ee8cdc96a9a3fd9165c12
Zachery Hostens <zacheryph@gmail.com>
2017-12-12 22:34:41 -0600

    add ability to track escrow
    Signed-off-by: Zachery Hostens <zacheryph@gmail.com>

--- loan/main.go
+++ loan/main.go
@@ -17,10 +17,11 @@ func (m Money) String() string {
 
 var (
 	amount   = flag.Float64("amount", 0.0, "loan amount")
+	escrow   = flag.Float64("escrow", 0.0, "amount going into escrow until 20% paid (only for -table)")
 	interest = flag.Float64("rate", 0.0, "interest rate")
-	months   = flag.Int("months", 360, "length of the loan (360 = 30 years)")
-	table    = flag.Bool("table", false, "print amortization table")
 	payment  = flag.Float64("payment", 0.0, "override the payment amount")
+	table    = flag.Bool("table", false, "print amortization table")
+	years    = flag.Int("years", 30, "length of the loan")
 )
 
 func main() {
@@ -34,7 +35,7 @@ func main() {
 		printAmortizationTable()
 	} else {
 		periodic := (*interest / 100) / 12
-		discount := calculateDiscount(*interest/100, *months)
+		discount := calculateDiscount(*interest/100, *years)
 		payment := *amount / discount
 		interestPayment := *amount * periodic
 
@@ -44,9 +45,9 @@ func main() {
 	}
 }
 
-func calculateDiscount(interest float64, months int) float64 {
+func calculateDiscount(interest float64, years int) float64 {
 	periodic := interest / 12
-	daily := math.Pow(periodic+1, float64(months))
+	daily := math.Pow(periodic+1, float64(years*12))
 	return (daily - 1) / (periodic * daily)
 }
 
@@ -54,28 +55,37 @@ func printAmortizationTable() {
 	var totalInterest float64
 
 	periodic := (*interest / 100) / 12
-	discount := calculateDiscount(*interest/100, *months)
+	discount := calculateDiscount(*interest/100, *years)
 	balance := *amount
 	writer := tabwriter.NewWriter(os.Stdout, 0, 8, 2, ' ', 0)
 	monthlyPayment := *payment
 	if monthlyPayment == 0 {
-		monthlyPayment = balance / discount
+		monthlyPayment = (balance / discount) + *escrow
 	}
 
-	headers := "Period\tOpening\tPayment\tInterest\tPrincipal\tEnding\n"
+	headers := "Period\tOpening\tPayment\tInterest\tEscrow\tPrincipal\tEnding\n"
 	writer.Write([]byte(headers))
 
-	for period := 1; period <= *months && balance > 0; period++ {
+	for period := 1; period <= (*years*12) && balance > 0; period++ {
+		// remove escrow if we are over 20%
+		if *escrow > 0 && balance < ((*amount)*0.8) {
+			if *payment == 0 {
+				monthlyPayment = monthlyPayment - *escrow
+			}
+			*escrow = 0
+		}
+
+		escrowPayment := *escrow
 		interestPayment := balance * periodic
-		principalPayment := monthlyPayment - interestPayment
+		principalPayment := monthlyPayment - interestPayment - escrowPayment
 
 		if principalPayment > balance {
 			principalPayment = balance
 			monthlyPayment = interestPayment + principalPayment
 		}
 
-		line := fmt.Sprintf("%d\t%s\t%s\t%s\t%s\t%s\n", period,
-			Money(balance), Money(monthlyPayment), Money(interestPayment),
+		line := fmt.Sprintf("%d\t%s\t%s\t%s\t%s\t%s\t%s\n", period,
+			Money(balance), Money(monthlyPayment), Money(interestPayment), Money(escrowPayment),
 			Money(principalPayment), Money(balance-principalPayment))
 
 		totalInterest += interestPayment
